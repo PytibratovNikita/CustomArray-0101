8cd629838c1602d148ed58a9c06eaf59957a301d
Никита <@HOME-PC>
2023-03-09 23:01:28 +0300

    Добавьте файлы проекта.

---
+++ CustomArray МДК 0101/Program.cs
@@ -0,0 +1,27 @@
+﻿using CustomArray;
+
+var array = new CustomArray<int>();
+
+array.Add(1);
+array.Add(2);
+array.Add(3);
+array.Add(4);
+array.Add(5);
+
+Console.WriteLine("Инициализация массива :");
+array.Print();
+
+array.Delete(2);
+
+Console.WriteLine("Массив после удаления индекса 3 :");
+array.Print();
+
+int first = array.FindFirst(x => x % 3== 0);
+Console.WriteLine($"Первое чётное число: {first}");
+
+array.Swap(1, 2);
+
+Console.WriteLine("Массив после замены индексов 1 и 2 :");
+array.Print();
+
+Console.ReadKey();
